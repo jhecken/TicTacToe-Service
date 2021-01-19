437bd397abe7af5e5593cda120b91cccfc0a7e0b
John Heckenliable <jdheckenliable@gmail.com>
2021-01-19 10:14:38 -0800

    adds basic service structure

---
+++ ttt-service/ttt-service/Services/GameService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ttt_service.Models;
+
+namespace ttt_service.Services
+{
+    public class GameService
+    {
+        public GameModel NewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ttt-service/ttt-service/Services/IGameService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ttt_service.Services
+{
+    public class IGameService
+    {
+    }
+}
